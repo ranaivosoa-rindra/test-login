59dee90eebeb8da0f44d109ca6928625ec6d7ebd
Gérald Rindra RANAIVOSOA <ranaivosoarindra3@gmail.com>
2022-04-21 22:43:45 +0300

    front set

---
+++ src/services/Authentication.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhLG_XlL5DFhlCtOFD4YpfBuyURu207KU",
+
+  authDomain: "test-login-2e920.firebaseapp.com",
+
+  projectId: "test-login-2e920",
+
+  storageBucket: "test-login-2e920.appspot.com",
+
+  messagingSenderId: "422950673726",
+
+  appId: "1:422950673726:web:91c8e28591fb9bddc2e06f",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
